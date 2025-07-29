8f0ed7a713a93ae7ef175a35b87a5a7657b55d2b
Ismael Moniz <hismamoniz@gmail.com>
2025-07-29 13:51:05 +0100

    setup cmake build system

---
+++ target/CMakeFiles/epsw.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/epsw.dir/link.d"
+  "CMakeFiles/epsw.dir/src/main.c.o"
+  "CMakeFiles/epsw.dir/src/main.c.o.d"
+  "epsw"
+  "epsw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/epsw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ target/CMakeFiles/epsw.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for epsw.
+# This may be replaced when dependencies are built.
